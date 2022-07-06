e0c11fb36fd9bcfeebffe356c64ac09fedce0f82
kingstone254 <kingstonekings1@gmail.com>
2022-07-05 18:44:02 -0700

    0-putchar.c repeat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
 #include "main.h"
 /**
- *  main -main
- *  print putchar followed by new line
- *  Return: 0
+ * main - main
+ * print putchar followed by new line
+ * Return: 0
  */
 int main(void)
 {
-	char _putchar[] = "_putchar";
-	int i = 0;
-
-	while (_putchar[i] != '\0')
-	{
-		_putchar(_putchar[i]);
-		i++;
-	}
+	_putchar(95);
+	_putchar(112);
+	_putchar(117);
+	_putchar(116);
+	_putchar(99);
+	_putchar(104);
+	_putchar(97);
+	_putchar(114);
 	_putchar('\n');
-
 	return (0);
 }
