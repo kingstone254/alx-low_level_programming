b1347c7bbbb6834c494860a7e9b835f00dd50d83
kingstone254 <kingstonekings1@gmail.com>
2022-07-07 18:52:46 -0700

    5-more_numbers.correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
 #include "main.h"
 
 /**
- * more_numbers - nmbr fct
- * Return: no return
+ *  more_numbers - prints 0 - 14
+ *  Return: void
  */
 
 void more_numbers(void)
 {
-	int i, j;
-	for (i = 1; i <= 10; i++)
+	int i, k;
+
+	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (k = 0; k < 15; k++)
 		{
-			if (j >= 10)
-				_putchar('1');
-			_putchar(j % 10 + '0');
+			if (k >= 10)
+				_putchar((k / 10) + 48);
+			_putchar((k % 10) + 48);
 		}
 		_putchar('\n');
 	}
-}	
+}
