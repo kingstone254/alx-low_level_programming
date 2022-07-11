044145dad3518951e5c09d9a2ec672b393aff1c7
kingstone254 <kingstonekings1@gmail.com>
2022-07-11 16:11:08 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,10 +10,15 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
+	int srcLength = 0;
 
-	for (; src[i] != '\0'; i++)
-	{
+	while (dest[i] != '\0')
+		dest[i++] = '\0';
+	i = 0;
+	while (src[srcLength] != '\0')
+		srcLength++;
+	for (i = 0; i < srcLength; i++)
 		dest[i] = src[i];
-	}
+	dest[srcLength] = '\0';
 	return (dest);
 }
