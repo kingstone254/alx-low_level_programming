9d3973dc4c0bf868c713b13d94631d696650ce3c
kingstone254 <kingstonekings1@gmail.com>
2022-07-20 15:47:08 -0700

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,13 +2,27 @@
 
 /**
  * palind2 - obtains length of a
- *  @a: string
- *  @l: integer to count length
- *  Return: On success 1.
- *  On error, -1 is returned, and errno is set appropriately.
+ * @a: string
+ * @l: integer to count length
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
 int palind2(char *a, int l)
+{
+	if (*a == 0)
+		return (l - 1);
+	return (palind2(a + 1, l + 1));
+}
+/**
+ * palind3 - compares string vs string reverse
+ * @a: string
+ * @l: length
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int palind3(char *a, int l)
 {
 	if (*a != *(a + l))
 		return (0);
